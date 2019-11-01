05b0906ab198d2b189c0eeb08880df80d239fe7b
WestEast1st <bhns1270132@gn.iwasaki.ac.jp>
2019-11-01 19:41:44 +0900

    test用のinmemoryデータ

--- tostring/inmemory.go
+++ tostring/inmemory.go
@@ -3,10 +3,46 @@ package tostring
 import (
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
-var testRequest = http.Request{}
-var testResponse = http.Response{}
-var testBody = ioutil.NopCloser(strings.NewReader(""))
-var testHeader = http.Header{}
+var (
+	testRequest = http.Request{
+		Method: "GET",
+		URL: &url.URL{
+			Scheme:   "https",
+			Host:     "google.com",
+			Path:     "/search",
+			RawPath:  "/search",
+			RawQuery: "q=otnet",
+		},
+		Header:        testHeader,
+		Proto:         "HTTP/1.1",
+		ProtoMajor:    1,
+		ProtoMinor:    1,
+		Body:          nil,
+		ContentLength: 0,
+		Host:          "google.com",
+		Form:          nil,
+	}
+	testResponse = http.Response{
+		Status:     "200 OK",
+		StatusCode: 200,
+		Proto:      "HTTP/1.1",
+		ProtoMajor: 1,
+		ProtoMinor: 1,
+		Header: http.Header{
+			"Cache-Control":    []string{"max-age=0", "private", "must-revalidate"},
+			"Content-Encoding": []string{"gzip"},
+		},
+	}
+	testBodystring = "<!DOCTYPE html><html><header></header><body>Test Data</body></html>"
+	testBody       = ioutil.NopCloser(strings.NewReader(testBodystring))
+	testHeader     = http.Header{
+		"Host":            []string{"google.com"},
+		"accept":          []string{"text/html", "application/xhtml+xml", "application/xml;q=0.9", "image/webp", "image/apng", "*/*;q=0.8", "application/signed-exchange;v=b3"},
+		"accept-encoding": []string{"gzip", "deflate", "br"},
+		"accept-language": []string{"ja"},
+	}
+)
